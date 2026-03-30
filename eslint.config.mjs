bb58a11117b4a664ade00cfa8e3bee15d5597283
peter <peteradigeb@gmail.com>
2026-03-30 23:13:50 +0100

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,3 +5,14 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
 ]);
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
+    }
+  }
+];
