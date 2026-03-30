ae3fa9c8c6a0b020e9b60244be6ba1fdecebe253
peter <peteradigeb@gmail.com>
2026-03-30 21:57:44 +0100

    ci pipeline commits

---
+++ test.js
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+function add(a, b) {
+  return a + b;
+}
+
+// Simple test
+if (add(2, 2) !== 4) {
+  console.error("Test failed ❌");
+  process.exit(1);
+}
+
+console.log("Test passed ✅");
